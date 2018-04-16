1947ebb341394fbd0aad6b8ccbcbba10f6bce514
Sabrina Hartl <30804260+96Sabiii@users.noreply.github.com>
2018-04-16 17:41:24 +0200

    card Links

--- res/js/src/hp.js
+++ res/js/src/hp.js
@@ -1,3 +1,20 @@
-function init()  {
+/* eslint-env browser  */
+
+var hp = (function () {
+  "use strict";
+
+  	var that = {};
+
+	function init()  {
+	}
+
+	// function setCardListener() {
+	// 	document.getElementById("card1").addEventListener("click", function() {
+	// 		location.open("inhalt1.html");
+	// 	});
+	// }
 	
-}
+	that.init = init;
+	return that;
+
+}());
