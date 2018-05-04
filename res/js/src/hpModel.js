bc4117b8a7df37fb060b712502c802edbc9428de
Sabrina Hartl <30804260+96Sabiii@users.noreply.github.com>
2018-05-04 10:21:57 +0200

    structure
    model und die verschiedenen Views angelegt

---
+++ res/js/src/hpModel.js
@@ -0,0 +1,14 @@
+/* eslint-env browser */
+/* global EventPublisher */
+
+/**
+  * Hier werden die Hintergrunddaten berechnet
+  */
+var hp = hp || {};
+hp.hpModel = function() {
+  "use strict";
+  var that = new EventPublisher();
+
+  return that;
+
+};
---
+++ res/js/src/hpRelationView.js
@@ -0,0 +1,14 @@
+/* eslint-env browser */
+/* global EventPublisher */
+
+/**
+  * Hier werden die Hintergrunddaten berechnet
+  */
+var hp = hp || {};
+hp.hpRelationsView = function() {
+  "use strict";
+  var that = new EventPublisher();
+
+  return that;
+
+};
