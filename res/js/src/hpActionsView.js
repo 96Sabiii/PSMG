1c52b0fba2c0f2dc0a6eedebcc63635bea9978d6
Sabrina Hartl <30804260+96Sabiii@users.noreply.github.com>
2018-05-25 09:03:21 +0200

    bubble chart

--- res/js/src/hpActionsView.js
+++ res/js/src/hpActionsView.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 /* global EventPublisher */
+// based on: https://bost.ocks.org/mike/bar/
 
 /**
   * Hier werden die Hintergrunddaten berechnet
@@ -11,18 +12,58 @@ hp.hpActionsView = function() {
 
   function bubbleChart(){
     console.log("bubblechart");
-    d3.csv("C:/Users/cip/Documents/GitHub/PSMG/res/assets/data/elements-by-episode.csv", function(data) {
-    let radius = 0,
-    lastRadius;
-      var selection = d3.select("#myChart");
-      console.log(data);
-      var groups = selection.selectAll('g').data(data.columns).enter().append('g').append("circle")
-      .attr("r", function(c) { return  d3.sum(data, function(d) { return lastRadius = d[c]; }); })
-      .attr("cx", function(d) { return radius =+ lastRadius })
-      .attr("cy", "200px")
-      .style("fill", function() { return "hsl(" + Math.random() * 360 + ",100%,50%)"; });
-    });
+    doChart();
   }
+
+function doChart() {
+    d3.csv("../../assets/data/elements-by-episode.csv", function(error, data) {
+        createBubbleData(data);
+    });
+}
+
+
+// Für 2.2
+function createBubbleData(data) {
+    "use strict";
+    var chart = document.getElementById("myChart");
+    chart.style.height = "800";
+    chart.style.width = "800";
+    var object = {name: "root", children: []},
+        selection = d3.select("#myChart"),
+        g = selection.append("g").attr("transform", "translate(2,2)"),
+        colorCircles = d3.scaleOrdinal(d3.schemeCategory20);
+    
+    //daten umwandeln
+    for(var i = 2; i < data.columns.length; i++) {
+        let name = data.columns[i], value = 0;
+        for (var j = 1; j < data.length; j++) {
+            value = value + +data[j][name];
+        }
+        let el = {name,value}; 
+        object.children.push(el); 
+    }
+    
+    //Diagramm erstellen
+    var nodeFkt = d3.pack().size([700, 700]);
+    
+    var root = d3.hierarchy(object)
+        .sum(function(d) { return d.value; })
+        .sort(function(a, b) { return b.value - a.value; });
+    
+    nodeFkt(root);
+    
+    console.log(root);
+    //SVG erstellen
+    var nodes = g.selectAll(".node")
+    .data(root.descendants())
+    .enter().append("g")
+      .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    var circles = nodes.append("circle").attr("r", function(d) {return d.r })
+        .style("fill", function(d) {return colorCircles(d.value)} );
+    var labels = nodes.append("text").text(function(d) {return d.data.name });
+    
+}
 
   that.bubbleChart = bubbleChart;
   return that;
